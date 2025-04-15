dd3bffe917a16893c3f4f1f2e338542252340bb0
joshbrod <joshbrod@umich.edu>
2025-04-15 16:34:49 -0400

    undid some issues

--- predictive_model/neural_network.py
+++ predictive_model/neural_network.py
@@ -70,54 +70,54 @@ if __name__ == "__main__":
     df[columns] = scaler.fit_transform(df[columns])
     joblib.dump(scaler, 'scaler.pkl')
     # Split data for X,y
-    # X = torch.tensor(df[columns].values, dtype=torch.float32)
-    # y = torch.tensor(df["EAL"].values, dtype=torch.float32)
+    X = torch.tensor(df[columns].values, dtype=torch.float32)
+    y = torch.tensor(df["EAL"].values, dtype=torch.float32)
 
-    # X_train, X_test, y_train, y_test, location_train, location_test = train_test_split(X, y, location, test_size=0.2)
+    X_train, X_test, y_train, y_test, location_train, location_test = train_test_split(X, y, location, test_size=0.2)
 
-    # # Output actual list
-    # sorted_data = sorted(zip(y_test, location_test), key=lambda x: x[0], reverse=True)
-    # with open("list.txt", "w") as f:
-    #     for label, location in sorted_data:
-    #         f.write(f"Location: {location}, Label: {label}\n")
+    # Output actual list
+    sorted_data = sorted(zip(y_test, location_test), key=lambda x: x[0], reverse=True)
+    with open("list.txt", "w") as f:
+        for label, location in sorted_data:
+            f.write(f"Location: {location}, Label: {label}\n")
     
-    # # Create model
-    # model = Neural_Network()
-    # for name, param in model.named_parameters():
-    #     print(name, param.shape)
+    # Create model
+    model = Neural_Network()
+    for name, param in model.named_parameters():
+        print(name, param.shape)
 
-    # # Set hyper parameters 
-    # params = model.parameters()
-    # learning_rate = 1e-3
-    # criterion = torch.nn.MSELoss()
-    # optimizer = torch.optim.Adam(params=params, lr=learning_rate, weight_decay=0.01)
+    # Set hyper parameters 
+    params = model.parameters()
+    learning_rate = 1e-3
+    criterion = torch.nn.MSELoss()
+    optimizer = torch.optim.Adam(params=params, lr=learning_rate, weight_decay=0.01)
 
-    # train_nn(model, X_train, y_train, optimizer, criterion)
-    # print("Finished training!")
+    train_nn(model, X_train, y_train, optimizer, criterion)
+    print("Finished training!")
 
 
-    # # Calculate performance
-    # with torch.no_grad():  
-    #     predictions = model(X_test)
+    # Calculate performance
+    with torch.no_grad():  
+        predictions = model(X_test)
 
    
-    # predictions = predictions.squeeze() 
-    # predictions = predictions.cpu().numpy()
-    # y_test = y_test.cpu().numpy()
+    predictions = predictions.squeeze() 
+    predictions = predictions.cpu().numpy()
+    y_test = y_test.cpu().numpy()
 
-    # mse = mean_squared_error(y_test, predictions)
-    # rmse = np.sqrt(mse)
-    # r2 = r2_score(y_test, predictions)
+    mse = mean_squared_error(y_test, predictions)
+    rmse = np.sqrt(mse)
+    r2 = r2_score(y_test, predictions)
 
-    # print(f'Mean Squared Error: {mse:.4f}')
-    # print(f'Root Mean Squared Error: {rmse:.4f}')
-    # print(f'R² (Coefficient of Determination): {r2:.4f}')
-    # torch.save(model, "predictive_model/model.pth")
+    print(f'Mean Squared Error: {mse:.4f}')
+    print(f'Root Mean Squared Error: {rmse:.4f}')
+    print(f'R² (Coefficient of Determination): {r2:.4f}')
+    torch.save(model, "predictive_model/model.pth")
 
-    # # Output predicted list
-    # sorted_data = sorted(zip(predictions, location_test), key=lambda x: x[0], reverse=True)
-    # with open("predicted_list.txt", "w") as f:
-    #     for label, location in sorted_data:
-    #         f.write(f"Location: {location}, Label: {label}\n")
+    # Output predicted list
+    sorted_data = sorted(zip(predictions, location_test), key=lambda x: x[0], reverse=True)
+    with open("predicted_list.txt", "w") as f:
+        for label, location in sorted_data:
+            f.write(f"Location: {location}, Label: {label}\n")
 
             
